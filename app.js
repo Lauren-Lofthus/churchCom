777b8301fde0dbc95829c7cc7c3677097f29b88a
Lauren-Lofthus <147899987+Lauren-Lofthus@users.noreply.github.com>
2024-04-01 10:28:31 -0700

    Initial Files
    Uploaded intial HTML, CSS, and JS from VS Code

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function myEvent() {
+    var showEvent = document.getElementById("event_Only");
+    if (showEvent.style.display === "none") {
+        showEvent.style.display = "block"; }
+        else {
+            showEvent.style.display = "none";
+        }
+    }
